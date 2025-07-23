caa48314329b1537c5037e98b4ca4a6bdd539618
anupamk537@gmail.com <anupamk537@gmail.com>
2025-07-23 16:19:18 +0530

    first commit

---
+++ src/main/java/com/psabranchtest/psabranchtest/PsabranchtestApplication.java
@@ -0,0 +1,13 @@
+package com.psabranchtest.psabranchtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsabranchtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsabranchtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psabranchtest/psabranchtest/PsabranchtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psabranchtest.psabranchtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsabranchtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
